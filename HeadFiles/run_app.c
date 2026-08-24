82ff469895a11f71fb136c040fabf02b2ffe133e
Robin-JohnCo <zoulihua501@gmail.com>
2026-08-24 18:19:46 +0800

    head

--- HeadFiles/run_app.c
+++ HeadFiles/run_app.c
@@ -5,8 +5,9 @@ int main() {
 
 	int res1 = add(1, 2);
 	int res2 = mul(2, 3);
+	int res3 = add(3, 4);
 
-	printf("add : %d\nmul : %d\n", res1, res2);
+	printf("add : %d\nmul : %d\nadd3 : %d\n", res1, res2, res3);
 
 	return 0;
 }
